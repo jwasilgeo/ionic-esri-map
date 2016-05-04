de436100e048bbc77dd07424a729beedbaa63139
Andy Gup <agup2001@msn.com>
2016-05-04 10:46:53 -0600

    commit new and old .js files into new dirs

---
+++ www/js/tabbed/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope) {})
+
+.controller('EsriCtrl', function(/*esriLoader*/) {
+  // var self = this;
+
+  // esriLoader.require(['esri/Map'], function(Map) {
+  //   self.map = new Map({
+  //     basemap: 'topo'
+  //   });
+  // });
+});
